fe2fb7ebb27a5dcbd94a63c518f4b9eadf1e79b9
diazarely <102704288+diazarely@users.noreply.github.com>
2022-04-18 13:22:25 -0700

    submission

--- review.js
+++ review.js
@@ -20,15 +20,16 @@
 // =================================================
 
 // 1.) Declare a variable
-
+let name
 // 2.) Change the value of this variable
-
+let name = true
 // 3.) Change the data type of this variable
-
+let name = 'Arely'
 // 4.) Create another variable and the one from above to concatenate
-
+let fullName = name + Diaz
 // 5.) Use any of the variables above or create new ones and print using string interpolation
-
+let lastName = 'Diaz'
+console.log('My last name is $[lastName]')
 // ================ Variable Names =================
 /* 
     • Case-sentive! Try it out! :O
@@ -44,69 +45,71 @@
 // Look up the following string methods!
 
 // Using charAt(), print the letter i in Sabrina
-
+console.log('Sabrina'.charAt(4))
 // Define Unicode in a comment, and get the Unicode using charCodeAt() of the third character in Teddy
-
+console.log('Teddy'.charCodeAt(2))
 // Using fromCharCode() - make it readable for us :). You'll see!
-
+console.log(string.fromCharCode('Teddy'.charCodeAt(2)))
 // Take your first and last name and concat()
-
+console.log(name.concat('',lastName))
 // Create a string and make it return true using startsWith()
-
+console.log(name.startsWith('A'))
 // Now use any variable with endsWith() and return false
-
+console.log(name.endsWith('r'))
 // Finish the following sentence. Use includes() and return true.
 const ozgur = 'Once upon a time Ozgur looked up at the Moon, '
-
+console.log(ozgur.includes('saw'))
 // Help! I'm looking for my cow! Using indexOf() help Josh find his cow. What's the index of cow?
 let joshHadALittleLambOopsCow = "Josh lives in a farm. Josh was overwhelmed with coding. Josh is stressed out. Josh fed the chickens this morning. He watered his plants. He took his cow Betsy on a walk. Josh went home without Betsy. Josh went to bed that night, and realized he went home alone."
-
+console.log(joshHadALittleLambOopsCow.indexOf('cow'))
 // Oh no! The alien doppelgangers are here. Let's use our laserbeam lastIndexOf() to shoot down the last alien named Vanessa in the String Universe.
 
 const vanessa = "I am the Alpha Vanessa. I am the Omega Vanessa. We do not come in peace."
-
+console.log(vanessa.lastIndexOf('Vanessa'))
 // Can we use length for strings? I don't know, you tell me.
 const noWeCantTeo = "but did you try it out though?"
-
+console.log(noWeCantTeo.length)
 // If only it was this easy to replace() my ex. Totally not joking, LOL. JK
 const replaceGokuWithVegeta = "For my DBZ fans, we all know Goku is the best!"
-
+console.log(replaceGokuWithVegeta.replace('paku'))
 // Josh has been forgetful lately. Let's help Josh by using search(), and find their right 'wifey'. 
 const joshIsLookingForWifey = "WIFEY 1, wifey 2, WiFeY 3, Wifey 4"
-
+console.log(joshIsLookingForWifey.search('wifey'))
 // I guess we can share this pizza sentence. Use slice() to return the other half of pizza.
 const pizzaSentence = "pizza, other half of pizza"
-
+console.log(pizzaSentence.slice(7))
 // Now using the pizza sentence, return only pizza (before the comma)
-
+console.log(pizzaSentence.slice(0,5))
 // Okay, but who decided to go to this expensive restaurant? Yaz, Poornima, and Leshawn decided to split() the bill. Return an array separating Yaz, Poornima, and Leshawn.
 const splitTheBill = "Yaz, Poornima, and Leshawn"
-
+console.log(splitTheBill.split(','))
 // OH NO! The alien invaders from earlier were so against bill splitting so they decided to split() Yaz, Poornima, and Leshawn into pieces. Using splitTheBill, return an array separating all three by characters.
-
+console.log(splitTheBill.split(''))
 // Use this toLowerCase()
 const angry = "LOWER YOUR VOICE DOWN IF YOU AIN'T PAYING MY BILLS."
+console.log(angry.toLowerCase())
 // Tuan was angry today. Create a string with words that Tuan would yell out. Now lowercase it, to tell Tuan to chill out. 
-
+const tuan = 'HELLO EVERYONE'
+console.log(tuan.toLowerCase())
 // toUpperCase()
 const jahlunSaidToPutSomeRespectToHisName = "jahlun"
-
-// substring()
+console.log(jahlunSaidToPutSomeRespectToHisName.toUpperCase())
+// substring
 // on the chopping block...
 
 // Returns "ell"
 const basicGreeting = "Hello World"
-
+console.log(basicGreeting.substring(1,4))
 // Returns "JavaScript"
 const ohReally = "JavaScript Substring"
-
+console.log(ohReally.substring(0,10))
 // Returns aol.com
 const aslDays = "xXteoWuzHereXx@aol.com"
-
+console.log(aslDays.substring(aslDays.length-7))
 // trim()
 // If only I can use this for my love handles...
 // Create a variable with a string value with some extra loving on both sides.
-
+console.log('   This is an example of trim   '.trim())
 // =================================================
 //                     BOOLEAN
 // =================================================
@@ -120,24 +123,24 @@ const b = 10;
 const c = 15;
 const d = "Keke"
 
-// console.log(a _ b)
-// console.log(c _ b)
-// console.log(d _ d)
-// console.log(d _ a)
-// console.log(a _ 15)
-// console.log(a _ b _ c)
-// console.log(c _ b _ a _ d)
+console.log(a !== b)
+ console.log(c !== b)
+ console.log(d === d)
+ console.log(d !== a)
+ console.log(a !== 15)
+ console.log(a + b ==c)
+ console.log(c + b + a !== d)
 
 // =================================================
 //                     LOOPS
 // =================================================
 
 // Ken has a headache today and he said the room has been spinning like crazy. Make Ken's room spin 10 times using FOR LOOP. Make him print a sentence.
-
+for(let i= 0 ; i>=10 ; i++){console.log('The room has been soinning like crazy')}
 // Harry said that was so mean of Teo to do Ken dirty like that. Make Teo's vision spin 20 times FOR LOOP.
-
+for(let i= 0; i>=20 ; i++){console.log('That was so mean of Teo to do Ken dirty like that')}
 // But wait! Lisa said, while Teo is spinning 20 times. Let's make him say "I'm sorry". Print I'm sorry 20 times using a WHILE loop.
-
+let i= 0; while(i<=20){console.log("i'm sorry"); i++}
 // For the next three, look for the syntax of FOR IN and FOR OF loop. Very easy to understand syntax, and very clean :) 
 
 // To The Left! To The Left! Everything I want in the console to the left. Print thing1, thing2, thing 3 using (FOR IN LOOP)
@@ -149,78 +152,104 @@ const whateverQueenBeySaid = {
 
 // Using (FOR IN LOOP), print the indexes of the array.
 const lana = ['l', 'a', 'n', 'a']
-
+for(ch in lana){console.log(ch)}
 // USE (FOR OF LOOP)!
 const tia = ['teo', 'the', 'syntax', 'of', 'this', 'is', 'cleaner']
-
+for(item of tia){console.log(item)}
 // Look into forEach() loop, it accepts a callback aka a function inside a function. 
 // Swathi said don't forget each person. No person left behind! Use forEach() to iterate over the array perscholas. Inside the function, create a variable with the string "PS". Print their name in the console concatenated with the variable. 
 const perscholas = ['Amira', 'Arely', 'Jonathan']
-
+perscholas.forEach(item => console.log('PS: ${item'))
 // =================================================
 //                     ARRAYS
 // =================================================
-
+let participants = ["Arely", "Ozgur", "Swathi", "Touseef"]
+console.log(participants)
 // Look up the following: Make a note of what each one of these methods do
 // push()
+participants.push("Teo")
+console.log("After push method" + participants)
 // pop()
+participants.pop()
+console.log("After pop method" + participants)
 // unshift()
+participants.unshift("Teo")
+console.log("After unshift method" + participants)
 // shift()
+participants.shift()
+console.log("After shift method" + participants)
 // concat()
+let instructors = ["Teo","Joshua"]
+console.log(participants.concat(instructors))
 // splice()
+participants.splice(1,0, "New student")
+console.log(participants)
 // slice()
+console.log("This is a slice form -2", participants.slice(-2))
 // sort()
+console.log("This is a sort", participants.sort())
 // includes()
+console.log("This includes Ozgur", participants.includes("Ozgur"))
 // indexOf()
+console.log("what is the index of Swathi", participants.indexOf('Swathi'))
 // length
-
+console.log("Lenght of participant array is ", participants.length)
 const fruits = ['apple', 'banana', 'orange']
 
 // Print banana
+console.log(fruits[1])
 
 // Let's turn it up a notch. I won't tell you what array methods to use. 
 
 // Join all the elements of the array into a string separated by a space.
-
+console.log(fruits.join(' '))
 // Remove orange
-
+console.log(fruits.pop())
 // Add strawberry, kiwi, and grapes at the end
-
+console.log(fruits.push("strawberry","kiwi","grapes"))
 // Remove apple
-
+console.log(fruits.shift())
 // Add mango at the beginning of the array
-
+console.log(fruits.unshift("mango "))
 // Add lemon, and grapefruit between mango and banana
-
+console.log(fruits.splice(1,0, "lemon" , "grapefruit"))
 // Remove banana and strawberry
-
+console.log(fruits.splice(3,2))
 // Create a new array called exoticFruits. Add 3 exotic fruits inside of this array. Create a new variable and assign it with the value of concatinating fruits array and exoticFruits array.
-
+const exoticFruits = ['durian', 'lucana','sapodilla']
+let allFruits = fruits.concat(exoticFruits)
 // Print the last two exotic fruits without altering the newly concatenated array.
-
+console.log(allFruits.slice(-2))
 // Monalissa said she needs help re-organizing her items in alphabetical order.
 const monalissaIsMessy = ["pencil", "paper", "notebook", "computer"]
-
+console.log(monalissaIsMessy.sort())
 // Kevin accidentally stepped into the mirror world and needs help reversing his words.
 const mirrorMirrorOnTheWall = ["all", "them", "of", "greatest", "the", "is", "who"]
-
+console.log(mirrorMirrorOnTheWall.reverse())
 // ===== HIGHER ORDER METHODS =====
 // Rafael said he''s trying to stop cursing so much, and he wants to start with his text messages. Help him write code to eliminate the badword in his sentences. Must return a string sentence. Must use filter(). :) 
 const RafaelNoBadWords = "I am so tired of this badword food. I do not want this badword badword food."
+console.log(RafaelNoBadWords.split(' ').filter(word => !word.includes('badword')).join(" "))
 
 // Man math is hard. Let's make our computer do it for us. Use reduce() to find the total of the numbers in numbersToAddUp
 const numbersToAddUp = [99, 5677, 232, 84, 2, 231]
+console.log(numbersToAddUp.reduce((x,y) => x+y))
 
 // ===== SPREAD OPERATOR ===== 
 
 // Create a variable and assign a value of a copied array using the spread operator
-
+const arrOne = ["hello", "goodbye"]
+const arrTwo = [...arrOne ]
 // Create two arrays with any elements and connect both of them using the spread operator. Save the connected arrays into a new variable.
-
+const daysOne =["saturday", "sunday"]
+const daysTwo =["monday", "tuesday"]
+const allDays =[...daysOne, ...daysTwo]
+console.log(allDays)
 // Using the variable with the newly connected arrays, use spread operator to add something at the end.
-
+allDays.push("wednesday")
+console.log(allDays)
 // Using the variable with the newly connected arrays, use spread operator to add something at the beginning.
-
+**
 // ===== ACCESS =====
 
 // Donut Shopping
@@ -246,7 +275,9 @@ const donutShop =[
 ]
 
 // Anthony wants applecrumble. Print please!
+console.log(donutShop[1][0][1])
 // Tosi wants ihatethis. :) Print!
+console.log(donutShop[2][0][0][1][0])
 
 // =================================================
 //                     IF/ELSE
@@ -254,11 +285,39 @@ const donutShop =[
 
 // Teo is a person. I would hope so...
 // Check if Teo is a person. If true, print 'Teo is a person!' if not then print 'Teo is not a person!'
+const person = "teo" 
+if (person === "teo"){
+  console.log("Teo is a person!") 
+}
+  else {
+    console.log("Teo is not a person!")
+  }
 // If Teo is a person, check to see if he is a male or a female. If male, print 'You got it right!' if not then print 'Wrong. Teo is going to remove you from the class.'.
 // If male, check to see if Teo is hungry. If Teo is 'hungry' then print 'Let's buy Teo some tacos!'. If Teo is 'not hungry' then print 'He doesn't need to eat.'. Anything else, print 'If Teo is not hungry, am I hungry?'
-
+let person = 'teo', gender = 'male', hunger = false
+if (person === 'teo'){
+    console.log("Teo is a person")
+    if (gender === 'male'){
+      console.log("You got it right!")
+      if(hunger === true){
+          console.log("Let's buy Teo some Tacos!")
+      }else{
+        console.log("He doesn't need to eat")
+      }
+    }else{
+      console.log("Wrong. Teo is going to remove you from the class.")
+    }
+  }else {
+    console.log("Teo is not a Person")
+  }
 // Create a if/else ternary to check if Gustavo is the coolest. If coolest, print 'You got that right!', else print 'James wants to argue. He says he's the best!'
-
+let gustavo = coolest 
+if (gustavo === coolest){
+  console.log('You got that right!')
+}
+else {
+  console.log("James wants to argue. He says he's the best!")
+}
 // =================================================
 //                     FUNCTIONS
 // =================================================
@@ -266,11 +325,23 @@ const donutShop =[
 // Don't forget to call your functions :)
 
 // Create a function called sayGreeting and PRINT 'Hello, ma'amsir!'
+function sayGreeting () {
 
+  console.log ("Hello, ma'amsir!")
+}
+sayGreeting()
 // Create a function that takes a string as a parameter. PRINT 'This is my (STRING)'. Please replace the (STRING) with the parameter. Don't play with me haha.
-
+function fun (string) {
+  console.log("this is my",string)
+}
+fun("function ")
 // Create a function called add that takes in three number parameters. Create 3 variables and assign numerical values to them. Invoke your add() and pass in those three variables as arguments.
 
+function add (num1,num2,num3){
+   console.log('your numbers are $[num1] $[num2] $[num3]')
+  }
+  
+  add(1, 2, 3)
 // Create a function called fightClub() that accepts a name parameter.  If the name:
 // 'Teo' RETURN '1st rule: You do not talk about Fight Club.'
 // 'Manara' RETURN '2nd rule: YOU DO NOT TALK ABOUT FIGHT CLUB.'
@@ -278,24 +349,53 @@ const donutShop =[
 // 'Devin' RETURN '4th rule: Only two guys to a fight.'
 // Anything else, RETURN 'No shirts
 
+const myClub = function fightClub(name){
+if (name === "Teo") {
+  return '1st rule: You do not talk about Fight Club.'
+} else if (name === 'Manara'){
+return '2nd rule: YOU DO NOT TALK ABOUT FIGHT CLUB.'
+} else if (name === 'Liv'){
+  return '3rd rule: If someone yells "Stop!", goes limp, taps out, the fight is over.'
+} else if (name === 'Devin'){
+  return '4th rule: Only two guys to a fight.'
+} else {
+  return "No shirts"
+}
+}
+console.log(myClub('manara'))
 // Create a function called hello() that prints 'Hello'. Return an anonymous function inside hello() and have it print 'Goodbye'. Invoke your hello function in some way to see Hello and Goodbye in the console.
-
+function hello(){
+  console.log('hello'+' '+numbersToAddUp('Goodbye'))
+  function nest(a){
+return(a)}
+}
+hello()
 // Create a function expression with your first name as the variable and then print your first and last name
-
+const Arely = function me (){
+  console.log("Arely Daiz")
+}
+Arely()
 // Create an arrow function that accepts a number and have it return that number doubled
-
+const arrow = (nee) => {
+  return nee *2
+}
+console.log(arrow(7))
 // =================================================
 //                     OBJECTS
 // =================================================
 
 // Create an object and call it human. Add a name, age, and location property. Give the properties values.
-
+let human = {
+  name : "Arely",
+  age : 20,
+  location : "Dallas"
+}
 // Access the name using dot notation
-
+console.log(human.name)
 // Access the age using square brackets
-
+console.log(human["age"])
 // Use object destructuring to access location
-
+console.log(human.location)
 // ACCESS Granted
 const bulbasaur = {
   name: 'Bulbasaur',
@@ -314,17 +414,23 @@ const bulbasaur = {
 }
 
 // Print overgrow
-
+console.log(bulbasur.abilities[0])
 // Print cut
-
+console.log(bulbasaur.moves[2])
 // Print Bulbahhhh!!!!!
-
+console.log(bulbasaur.sound(true))
 // Add a height of 7 to bulbasaur using the dot notation. (Don't change bulbasaur object manually)
-
+console.log(bulbasaur.push("height : 7"))
 // Add a property called order and assign it a value of 1 using the square brackets. (Don't change bulbasaur object manually)
 
 // Print an array that contains every single properties in bulbasaur
-
+let bulbasaurTwo = bulbasaur["name"]["abilities"]["moves"]
+console.log(bulbasaurTwo)
 // Print every single properties one by one in the console
-
+console.log(bulbasaur.name)
+console.log(bulbasaur.abilities)
+console.log(bulbasaur.moves)
+console.log(bulbasaur.sound(true))
 // Print an array that contains every single values in bulbasaur
+let length = bulbasaur.length()
+console.log(length)
